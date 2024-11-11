447960f873db7dff871f09e93beb2296644f80dc
Ajayreddy99 <ajaykumarreddysammeta@my.unt.edu>
2024-11-11 12:21:50 -0600

    Add files via upload
    login pages for checkout have been done and confirmation pages

---
+++ src/frontend/componenets/cart.js
@@ -0,0 +1,57 @@
+// Updated CartPage.js
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../styling/cart.css";
+
+const CartPage = () => {
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const [cart, setCart] = useState(state?.cart || []);
+  const userData = state?.userData || {};
+
+  const handleBackToMedicine = () => {
+    const cartSaved = cart;
+    navigate("/medicines", { state: { cartSaved, userData } });
+  };
+
+  const handleCheckout = () => {
+    const totalPrice = cart
+      .reduce((acc, item) => acc + item.price * item.quantity, 0)
+      .toFixed(2);
+    navigate("/checkout-confirmation", {
+      state: { cart, userData, totalPrice },
+    });
+  };
+
+  const totalPrice = cart
+    .reduce((acc, item) => acc + item.price * item.quantity, 0)
+    .toFixed(2);
+
+  return (
+    <div className="cart-container">
+      <h1>Your Cart</h1>
+      {cart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <>
+          <ul>
+            {cart.map((item) => (
+              <li className="card-li" key={item.id}>
+                <h3>{item.name}</h3>
+                <p>Quantity: {item.quantity}</p>
+                <p>Rs {item.price * item.quantity}</p>
+              </li>
+            ))}
+          </ul>
+          <h3>Total: Rs {totalPrice}</h3>
+          <button onClick={handleCheckout}>Proceed to Checkout</button>
+        </>
+      )}
+      <button onClick={handleBackToMedicine} className="cart-button">
+        Back to Medicines
+      </button>
+    </div>
+  );
+};
+
+export default CartPage;
