d8666ee1dae19aa66b62185493f9f62772cf93e7
ganesh <gnevercodes@gmail.com>
2024-11-11 20:02:18 -0600

    Payment and Mobile Res

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51QK38OInmpkINd3bjGzGsllPn2Sxwb38CbE0YXvBz2HyNm9MBxyfDg0Xx6ep9dWDvNmNynBvgtuU97iEkvTebstL00sqJNGztT"
+);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // replace with your frontend's URL
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
@@ -43,6 +51,26 @@ app.use(appointmentRouter);
 app.use(bloodDonorRouter);
 app.use(adminRouter);
 
+app.post("/api/create-payment-intent", async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    // Create a payment intent with the specified amount
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+    console.log(paymentIntent.status);
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+    // console.log(res);
+  } catch (error) {
+    console.error("Error creating payment intent:", error);
+    res.status(500).json({ error: "Failed to create payment intent" });
+  }
+});
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
